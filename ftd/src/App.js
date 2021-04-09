ba9557f29b91e3773329fbefe897ffd296e866dd
abdullahfrq <abdullah.farouq2010@gmail.com>
2021-04-09 05:01:19 +0000

    cleanup of App.js

--- ftd/src/App.js
+++ ftd/src/App.js
@@ -31,15 +31,3 @@ class App extends Component {
 
 }
 export default App;
-
-/* 
-    <head>
-      <meta charset="utf-8"/>
-      <meta HTTP-EQUIV="EXPIRES" CONTENT="-1"></meta>
-      <script src="jquery-3.5.1.min.js"></script>
-      <script language="javascript" src="model.js" > </script>
-      <script language="javascript" src="controller.js" > </script>
-      <link rel="stylesheet" type="text/css" href="style.css" />
-      <title>f0rt9it32d</title>
-	  </head>
-    */
